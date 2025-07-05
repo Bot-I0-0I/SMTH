55229f95b8576a53843d50cd293830e3563ba59e
Bot-I0-0I <grz4488684@gmail.com>
2025-07-05 19:03:26 +0500

    Add files via upload

---
+++ Web.js
@@ -0,0 +1,8 @@
+ function openDroplist (className) {
+ document.querySelector(className).style.display = 'block';
+}
+function closeDroplist (className) {
+ document.querySelector(className).style.display = 'none';
+}
+//NavBar Functions---------------------------------------------
+
